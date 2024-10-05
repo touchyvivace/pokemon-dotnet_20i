2c507b2acc7239a464b883f62b82c43e77666c45
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 10:17:57 +0700

    feat: implement ApiControllerBase to be a based class of all Controller

---
+++ pokemon-dotnet/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private ISender _mediator = null!;
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
--- pokemon-dotnet/Controllers/PokemonController.cs
+++ pokemon-dotnet/Controllers/PokemonController.cs
@@ -1,17 +1,9 @@
-using Pokemon.Data;
-using Pokemon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Pokemon.Domain.Entities;
 
 namespace Pokemon.Controllers {
-    [ApiController]
-    [Route("api/pokemon")]
-    public class PokemonController : ControllerBase {
-        private readonly ApiContext _context;
+    public class PokemonController : ApiControllerBase {
 
-        public PokemonController(ApiContext context) {
-            _context = context;
-        }
-        
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> GetAllPokemons()
