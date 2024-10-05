eda95602ecfb8086aaa1d3e2fc972986875e2e09
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 10:59:11 +0700

    DateTimeService for datetime.now calling

---
+++ Infra/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿
+using Pokemon.Core.Common.Interfaces;
+
+namespace BizViewV6Api.Infrastructure.Services;
+
+public class DateTimeService : IDateTime
+{
+    public DateTime Now => DateTime.Now;
+}
