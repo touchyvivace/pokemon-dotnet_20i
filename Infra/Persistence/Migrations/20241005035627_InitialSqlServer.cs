863ebd2713c592376ec058b4e558e4ba493b2c8e
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 10:58:44 +0700

    add dbmigrations

---
+++ Infra/Persistence/Migrations/20241005035627_InitialSqlServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PokemonName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PokemonType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    Shiny = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemon", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemon");
+        }
+    }
+}
