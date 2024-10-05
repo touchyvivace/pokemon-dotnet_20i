475d25f30b16838916f87d7786f8753a541e8375
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 09:58:10 +0700

    BREAKING CHANGE: remove APIContext and migrate to use ApplicationDbContext instead

---
+++ Infra/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Options;
+using Pokemon.Core.Common.Interfaces;
+using Pokemon.Domain.Entities;
+using Pokemon.Infrastructure.Persistence.Interceptors;
+
+namespace Infra.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly IMediator _mediator;
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IMediator mediator,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor)
+            : base(options)
+        {
+            _mediator = mediator;
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<PokemonInfo> Pokemon => Set<PokemonInfo>();
+
+        public new DatabaseFacade Database => base.Database;
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+        }
+
+
+
+    }
+}
