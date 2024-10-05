0ba291b5879de8b50f216b050b00090a1c609ef8
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 13:48:00 +0700

    add inmemoryDB

--- Infra/ConfigureServices.cs
+++ Infra/ConfigureServices.cs
@@ -1,10 +1,9 @@
 ﻿
-
-using BizViewV6Api.Infrastructure.Services;
 using Infra.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Pokemon.Core.Common.Interfaces;
+using Pokemon.Infra.Services;
 using Pokemon.Infrastructure.Persistence.Interceptors;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -16,11 +15,17 @@ public static class ConfigureServices
         services.AddScoped<AuditableEntitySaveChangesInterceptor>();
 
      
-
+        if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("pokemondb"));
+        }
+         else
+        {
             services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                     builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-
+        }
         services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
         services.AddScoped<ApplicationDbContextInitialiser>();
         services.AddMemoryCache();
