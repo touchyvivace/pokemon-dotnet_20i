3b939a5c9ce913278f239cd9b7ff57edfac239ea
Touchchai.A <touchchai.a@bis.co.th>
2024-10-05 11:52:06 +0700

    refactor

--- pokemon-dotnet/Controllers/PokemonController.cs
+++ pokemon-dotnet/Controllers/PokemonController.cs
@@ -2,40 +2,50 @@ using Core.Pokemons.Queries;
 using Microsoft.AspNetCore.Mvc;
 using Pokemon.Domain.Entities;
 
-namespace Pokemon.Controllers {
-    public class PokemonController : ApiControllerBase {
+namespace Pokemon.Controllers
+{
+    public class PokemonController : ApiControllerBase
+    {
 
         [HttpGet]
         [Route("")]
-        public async Task<IEnumerable<PokemonInfo>> GetAllPokemons([FromQuery] GetPokemonsQuery pokemonsQuery) 
+        public async Task<IEnumerable<PokemonInfo>> GetAllPokemons([FromQuery] GetPokemonsQuery pokemonsQuery)
         {
             return await Mediator.Send(pokemonsQuery);
         }
+        [HttpGet("{pokemonId}")]
+        public async Task<ActionResult<PokemonInfo>> GetPokemonById(int pokemonId)
+        {
+            var query = new GetPokemonByIdQuery { Id = pokemonId };
+            var result = await Mediator.Send(query);
 
-        [HttpGet]
-        [Route("{pokemonId}")]
-        public async Task<IActionResult> GetPokemonById() {
-            
-            return Ok();
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
-        
         [HttpPut]
         [Route("{pokemonId}")]
-        public async Task<IActionResult> UpdatePokemon() {
-            
+        public async Task<IActionResult> UpdatePokemon()
+        {
+
             return Ok();
         }
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<PokemonInfo>> CreatePokemon() {
+        public async Task<ActionResult<PokemonInfo>> CreatePokemon()
+        {
             return Ok();
         }
 
         [HttpDelete]
         [Route("{pokemonId}")]
-        public async Task<IActionResult> DeletePokemon() {
-            
+        public async Task<IActionResult> DeletePokemon()
+        {
+
             return Ok();
         }
     }
